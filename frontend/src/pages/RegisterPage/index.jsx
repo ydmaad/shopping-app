7a8a5b358c050dc95bd66337a0223956f59b16c1
Minae <ydmaad@gmail.com>
2025-08-28 20:57:56 +0900

    feat: tailwind 재설정, 회원가입 폼 생성

--- frontend/src/pages/RegisterPage/index.jsx
+++ frontend/src/pages/RegisterPage/index.jsx
@@ -1,7 +1,71 @@
 import React from "react";
 
 const RegisterPage = () => {
-  return <div>RegisterPage</div>;
+  return (
+    <>
+      <section className="flex flex-col justify-center mt-20 max-w-[400px] m-auto">
+        <div className="p-6 bg-whtie rounded-md shadow-md">
+          <h1 className="text-3xl font-semibold text-center">회원가입</h1>
+
+          <form className="mt-6">
+            <div className="mb-2">
+              <label
+                htmlFor="email"
+                className="text-sm font-semibold text-gray-800"
+              >
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                className="w-full px-4 py-2 mt-2 bg-white border rounded"
+              />
+            </div>
+            <div className="mb-2">
+              <label
+                htmlFor="name"
+                className="text-sm font-semibold text-gray-800"
+              >
+                Name
+              </label>
+              <input
+                type="text"
+                id="name"
+                className="w-full px-4 py-2 mt-2 bg-white border rounded"
+              />
+            </div>
+            <div className="mb-2">
+              <label
+                htmlFor="password"
+                className="text-sm font-semibold text-gray-800"
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                className="w-full px-4 py-2 mt-2 bg-white border rounded"
+              />
+            </div>
+            <div className="mt-6">
+              <button
+                type="submit"
+                className="w-full bg-black text-white px-4 py-2 rounded-md hover:bg-gray-700 duration-200"
+              >
+                회원가입
+              </button>
+            </div>
+            <p className="mt-8 text-xs font-light text-center text-gray-700">
+              아이디가 있다면?{" "}
+              <a href="/login" className="font-medium hover:underline">
+                로그인
+              </a>
+            </p>
+          </form>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default RegisterPage;
