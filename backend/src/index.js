ba3a3f1e9e741acd8d40195e7ae7ff6e5ae8fd93
Minae <ydmaad@gmail.com>
2025-08-28 18:29:05 +0900

    feat: 몽고db 연결

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,26 @@
 const express = require("express");
-
+const cors = require("cors");
 const app = express();
+const mongoose = require("mongoose");
 const port = 4000;
+const dotenv = require("dotenv");
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONG_URI)
+  .then(() => {
+    console.log("몽고디비 연결 완료!!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.get("/", (req, res) => {
   res.send("안녕하세영~~~~~~~~");
 });
+
+app.use(cors());
+app.use(express.json());
 
 app.listen(port, () => {
   console.log(`${port}번에서 실행이 되었습니다.`);
