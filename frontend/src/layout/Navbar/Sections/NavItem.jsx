e19ddb22d7745c552846863efc02094d48b57d23
Minae <ydmaad@gmail.com>
2025-08-29 20:21:26 +0900

    feat: 네이게이션 라우트 필드 추가

--- frontend/src/layout/Navbar/Sections/NavItem.jsx
+++ frontend/src/layout/Navbar/Sections/NavItem.jsx
@@ -2,11 +2,19 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import { logoutUser } from "../../../store/thunkFunctions";
-
+import { AiOutlineShoppingCart } from "react-icons/ai";
 const routes = [
   { to: "/login", name: "로그인", auth: false },
   { to: "/register", name: "회원가입", auth: false },
   { to: "", name: "로그아웃", auth: true },
+  { to: "/product/upload", name: "업로드", auth: true },
+  {
+    to: "/user/cart",
+    name: "카트",
+    auth: true,
+    icon: <AiOutlineShoppingCart style={{ fontSize: "1.4rem" }} />,
+  },
+  { to: "/history", name: "주문목록", auth: true },
 ];
 
 const NavItem = ({ mobile }) => {
@@ -36,6 +44,20 @@ const NavItem = ({ mobile }) => {
                 className="py-2 text-center border-b-4 cursor-pointer"
               >
                 <button onClick={handleLogout}>{route.name}</button>
+              </li>
+            );
+          } else if (route.name === "카트") {
+            return (
+              <li
+                key={route.name}
+                className="relative py-2 text-center border-b-4 cursor-pointer"
+              >
+                <Link to={route.to}>
+                  {route.icon}
+                  <span className="absolute top-0 inline-flex items-center justify-center w-4 h-4 text-xs font-bold text-white bg-red-500 border-2 border-white rounded-full -right-3">
+                    {1}
+                  </span>
+                </Link>
               </li>
             );
           } else {
